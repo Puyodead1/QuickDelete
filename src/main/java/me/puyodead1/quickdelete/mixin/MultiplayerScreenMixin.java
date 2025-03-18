8682197685c49a04c8358c1e4e9af6a63d70e80a
Puyodead1 <puyodead@proton.me>
2025-03-18 00:40:16 -0400

    Redo the mixin to fix duplicate button
    I noticed the vanilla delete button was still being created and was in the top left

--- src/main/java/me/puyodead1/quickdelete/mixin/MultiplayerScreenMixin.java
+++ src/main/java/me/puyodead1/quickdelete/mixin/MultiplayerScreenMixin.java
@@ -5,17 +5,14 @@ import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerServerListWidget;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableTextContent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(value = MultiplayerScreen.class, priority = 1)
 public abstract class MultiplayerScreenMixin extends Screen {
-
-    @Shadow
-    private ButtonWidget buttonDelete;
 
     @Shadow
     protected MultiplayerServerListWidget serverListWidget;
@@ -27,16 +24,28 @@ public abstract class MultiplayerScreenMixin extends Screen {
     @Shadow
     protected abstract void removeEntry(boolean confirmAction);
 
-    @Inject(method = "init", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/client/gui/screen/multiplayer/MultiplayerScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;", ordinal = 6))
-    protected void init(CallbackInfo ci) {
-        this.buttonDelete = this.addDrawableChild(ButtonWidget.builder(Text.translatable("selectServer.delete"), button -> {
-            MultiplayerServerListWidget.Entry entry = this.serverListWidget.getSelectedOrNull();
-            if (entry instanceof MultiplayerServerListWidget.ServerEntry) {
-                String string = ((MultiplayerServerListWidget.ServerEntry) entry).getServer().name;
-                if (string != null) {
-                    this.removeEntry(true);
-                }
+    @Redirect(
+            method = "init",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/widget/ButtonWidget;builder(Lnet/minecraft/text/Text;Lnet/minecraft/client/gui/widget/ButtonWidget$PressAction;)Lnet/minecraft/client/gui/widget/ButtonWidget$Builder;"
+            )
+    )
+    private ButtonWidget.Builder modifyDeleteButtonBuilder(Text text, ButtonWidget.PressAction originalAction) {
+        if (text.getContent() instanceof TranslatableTextContent translatableTextContent) {
+            if(translatableTextContent.getKey().equals("selectServer.delete")) {
+                return ButtonWidget.builder(text, (button) -> {
+                    MultiplayerServerListWidget.Entry entry = this.serverListWidget.getSelectedOrNull();
+                    if (entry instanceof MultiplayerServerListWidget.ServerEntry serverEntry) {
+                        String serverName = serverEntry.getServer().name;
+                        if (serverName != null) {
+                            this.removeEntry(true);
+                        }
+                    }
+                });
             }
-        }).width(74).build());
+        }
+
+        return ButtonWidget.builder(text, originalAction);
     }
 }
